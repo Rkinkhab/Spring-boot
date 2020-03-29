6510ecc3e9773b09693cba41ba089f480a469a09
Rajat Kinkhabwala <rkinkhab@stevens.edu>
2020-03-29 17:45:04 -0400

    Update fix_previleges.sh

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x /home/ec2-user/target/justiceleague-tracker-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/scripts/start_server.sh
-chmod +x /home/ec2-user/scripts/stop_server.sh
+chmod +x /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar
+chmod +x /home/ec2-user/start_server.sh
+chmod +x /home/ec2-user/stop_server.sh
 
